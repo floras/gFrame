9ea79207cca68f932eae12acd4dd4c0201c1ad57
Flora.dev <flora.dev@gmail.com>
2011-04-28 20:33:37 +0900

    global onOff button

--- demo/demo.js
+++ demo/demo.js
@@ -12,5 +12,11 @@ $(function(){
 	$("button:enabled").addClass('enabled');
 	$("<div class='overlay'></div>").appendTo($("#container"));
 	$("<div id='overlay-header' class='overlay'></div>").appendTo($("#title"));
-	if (!gFrame.alive) $("<div id='WoffButton'><div id='offButton'onclick='gFrame.start();'>OFF</div></div>").prependTo($("body"));
+	
+	if (window.gFrame&& window.gFrame.alive) {
+		var onOff = "<style>#offButton:hover{border:1px solid #66D399}</style><div id='offButton'onclick='gFrame.exit();' style='display:block;height:100%;background:#43C881;color:#fff;cursor:pointer;padding:15px;font:bold 14px arial;width:35px;height:20px'>ON</div>";	
+		var style = "border: 3px solid #43C881;border-radius:5px;text-align:center;";			
+		gFrame('switch', onOff, {top:10, right:25, style:style});
+	} else $("<div id='WoffButton'><div id='offButton'onclick='gFrame.start();'>OFF</div></div>").prependTo($("body"));
+	
 });
